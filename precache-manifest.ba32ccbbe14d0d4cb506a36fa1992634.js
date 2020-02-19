42639dfea1d6d571f1551166222d64b7a940320a
mileor <mileor@yandex.ru>
2020-02-19 10:01:09 +0300

    init

---
+++ precache-manifest.ba32ccbbe14d0d4cb506a36fa1992634.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "509a397ae920b6084a4ac8a162ac3b6a",
+    "url": "/affiche/index.html"
+  },
+  {
+    "revision": "933f8c98fd4aa5bef2a8",
+    "url": "/affiche/static/css/main.9e9dc1b4.chunk.css"
+  },
+  {
+    "revision": "7609ab728b69ae1efe2a",
+    "url": "/affiche/static/js/2.558780fa.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/affiche/static/js/2.558780fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "933f8c98fd4aa5bef2a8",
+    "url": "/affiche/static/js/main.fd9e37bb.chunk.js"
+  },
+  {
+    "revision": "507e87560cbb109d5ec7",
+    "url": "/affiche/static/js/runtime-main.7b6da3e1.js"
+  }
+]);
